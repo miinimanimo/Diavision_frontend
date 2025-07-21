b1fedd914cd5cd9d498fc9d18be8672bc4062845
miinimanimo <ssk07079@gmail.com>
2025-07-22 08:05:56 +0900

    라우팅 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login/Login';
 import PatientRegistration from './pages/PatientRegistration/PatientRegistration';
